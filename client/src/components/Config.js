274ddedb1c3063ad6e45d269278867c8bbbfd1c2
HYEONGWOO\jhw97 <jhw9790@naver.com>
2022-11-08 00:06:09 +0900

    API_KEY 추가

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,6 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'c325e598d73f32a6c0b69042ca49955a';
 
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
